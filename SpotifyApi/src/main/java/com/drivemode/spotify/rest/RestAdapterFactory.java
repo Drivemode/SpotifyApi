f65e9e3465c6105fe32d0d37ffd82bca46006e3e
KeithYokoma <Keith.Yokoma@gmail.com>
2015-01-15 19:30:22 +0900

    Fix executors for retrofit request and callback

--- SpotifyApi/src/main/java/com/drivemode/spotify/rest/RestAdapterFactory.java
+++ SpotifyApi/src/main/java/com/drivemode/spotify/rest/RestAdapterFactory.java
@@ -2,9 +2,6 @@ package com.drivemode.spotify.rest;
 
 import com.squareup.okhttp.OkHttpClient;
 
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-
 import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
 import retrofit.client.OkClient;
@@ -17,18 +14,15 @@ public class RestAdapterFactory {
     public static final String SPOTIFY_WEB_API_ENDPOINT = "https://api.spotify.com/v1";
     public static final String SPOTIFY_AUTHENTICATE_ENDPOINT = "https://accounts.spotify.com/";
     private final OkClient mOkClient;
-    private final Executor mExecutor;
 
     public RestAdapterFactory() {
         mOkClient = new OkClient(new OkHttpClient());
-        mExecutor = Executors.newSingleThreadExecutor();
     }
 
     public RestAdapter provideWebApiAdapter(RequestInterceptor interceptor) {
         return new RestAdapter.Builder()
                 .setLogLevel(RestAdapter.LogLevel.FULL)
                 .setClient(mOkClient)
-                .setExecutors(mExecutor, mExecutor)
                 .setEndpoint(SPOTIFY_WEB_API_ENDPOINT)
                 .setRequestInterceptor(interceptor)
                 .build();
@@ -38,7 +32,6 @@ public class RestAdapterFactory {
         return new RestAdapter.Builder()
                 .setLogLevel(RestAdapter.LogLevel.FULL)
                 .setClient(mOkClient)
-                .setExecutors(mExecutor, mExecutor)
                 .setEndpoint(SPOTIFY_AUTHENTICATE_ENDPOINT)
                 .build();
     }
